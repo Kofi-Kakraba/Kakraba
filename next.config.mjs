374f7d7f785c00c7b5396291783da78678feb326
Kofi Kakraba <benjamin.amoakwa@gmail.com>
2026-07-12 08:55:38 +0000

    config: authorization protocol for supabase image optimization optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    // This gives Next.js permission to optimize images coming from Supabase
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
